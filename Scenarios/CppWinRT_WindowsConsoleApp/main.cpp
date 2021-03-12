9bc32193809588ee7e4b218fe46c33be7f0c2d23
Joshua Larkin <jlarkin@microsoft.com>
2021-03-12 08:26:31 -0800

    add projects

---
+++ Scenarios/CppWinRT_WindowsConsoleApp/main.cpp
@@ -0,0 +1,15 @@
+﻿#include "pch.h"
+
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace CsWinRTComponent;
+
+int main()
+{
+    init_apartment();
+    printf("Starting up!\n");
+    Coord a = Coord();
+    Coord b = Coord(39.0, 80.0);
+    printf("We expect the distance between %ls and %ls to be 89.0\n", a.ToString().c_str(), b.ToString().c_str());
+    printf("Computed distance: %f", a.Distance(b));
+}
